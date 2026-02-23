85f22899f8c82a11280c1c4debd2cbacae088ed4
tsmh3939 <kingsfort327@gmail.com>
2026-02-23 20:49:29 +0900

    Revert "build: add postcss-purgecss to remove unused DaisyUI component styles"
    This reverts commit c3d54ae9a1f5040aea81e6686d52d3908cf7c4cc.

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,7 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-import cssnano from "cssnano";
-import purgecss from "@fullhuman/postcss-purgecss";
-
-const isProduction = process.env.NODE_ENV === "production";
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-    ...(isProduction
-      ? [
-          purgecss({
-            content: ["./src/**/*.{html,ts,tsx}"],
-            safelist: {
-              greedy: [/^\[data-theme/],
-            },
-            variables: false,
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          }),
-        ]
-      : []),
-    cssnano,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {},
+  },
 };
